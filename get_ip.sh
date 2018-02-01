ad9b6ba5145d9266665868f5eab5ecdaece8c100
sheng zhang <1150283637@qq.com>
2018-02-01 10:10:15 +0800

    2018年 02月 01日 星期四 10:10:15 CST

---
+++ get_ip.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /home/zs/langyxxl
+upnpc -l  |grep ExternalIPAddress |awk '{print $3}' >ipaddr.new
+diff ipaddr ipaddr.new
+if [[ $? != 0 ]]
+then
+    mv ipaddr.new ipaddr
+    git add -A
+    git commit -m `date`
+    git push
+fi
+
